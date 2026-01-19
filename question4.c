1bb942051bbcb2876805bc334f7b846e582e8b2d
Manu Kumar <manukumardsouza385@gmail.com>
2026-01-19 23:38:10 +0530

    Add files via upload

---
+++ question4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float f, c;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+    c = (f - 32) * 5 / 9;
+    printf("Temperature in Celsius=%f", c);
+    return 0;
+}
